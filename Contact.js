20a5d7f743cc9f7b6489ba503b6113aa77cb92dd
kshew2 <kshew2@uncc.edu>
2024-04-24 15:12:30 -0400

    Captcha

--- Contact.js
+++ Contact.js
@@ -6,9 +6,7 @@ const phone = document.getElementById("phone");
 function sendEmail() {
     const bodyMessage = `<b>Full Name:</b> ${fullName.value}<br> <b>Email:</b> ${email.value}<br> <b>Phone Number:</b> ${phone.value}`;
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "khalilshew1030@gmail.com",
-        Password : "1657036598AEF376CB18CCE9264C16378CCA",
+        SecureToken: "98d074a3-c922-434b-94e8-13801ed4a900",
         To : 'khalilshew1030@gmail.com',
         From : "khalilshew1030@gmail.com",
         Subject : "New Contact Submission",
@@ -26,9 +24,48 @@ function sendEmail() {
     );
 }
 
+function checkInputs(){
+  const items = document.querySelectorAll(".item");
+
+  for(const item of items){
+    if(item.value == ""){
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    if(items[1].value != ""){
+      checkEmail();
+    } 
+    items[1].addEventListener("keyup", () =>{
+      checkEmail();
+    })
+    item.addEventListener("keyup", () => {
+      if(item.value != ""){
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+      item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+function checkEmail(){
+  const emailRegex = /^([a-z\d\.\-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+  if(!email.value.match(emailRegex)){
+    email.classList.add("error");
+    email.parentElement.classList.add("error");
+  }
+  else {
+    email.classList.remove("error");
+    email.parentElement.classList.remove("error");
+  }
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
+    checkInputs()
     sendEmail();
 
 });
