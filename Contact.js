4476b9d57b3d9644702cf0d3c8a012744ce84c42
kshew2 <kshew2@uncc.edu>
2024-05-20 10:15:36 -0400

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -8,7 +8,7 @@ function sendEmail() {
     Email.send({
         SecureToken: "98d074a3-c922-434b-94e8-13801ed4a900",
         To : "khalilshew1030@gmail.com",
-        From : "Mitchellslawncare@gmx.com",
+        From : "khalilshew1030@gmail.com",
         Subject : "New Contact Submission",
         Body : bodyMessage
     }).then(
